903523c27f8c4a3682433169c091faa725c3051b
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2021-12-09 15:04:21 +0900

    update: main.c

--- main.c
+++ main.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+#include "type.h"
+
+unsigned get_count_countries(void);
+void get_countries_data(type_country_data *data);
+
 int main(void) {
-    return 0;
+    unsigned count_countries = get_count_countries();
+    struct _country_data countries_data[count_countries];
+    get_countries_data(countries_data);
+
+	return 0;
 }

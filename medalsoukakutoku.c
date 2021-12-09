eaa12f38cb09cd4615feb86cc10a125796db629b
mnissss <st20173ns@g.ibaraki-ct.ac.jp>
2021-12-09 15:41:43 +0900

    メダ総獲得数の比較

--- medalsoukakutoku.c
+++ medalsoukakutoku.c
@@ -9,14 +9,13 @@ int check_sum_medal(int ag, int as, int ab, int bg, int bs, int bb){
 	a_total = ag + as + ab;
 	b_total = bg + bs + bb;
 	
-	//aの国が大きい場合1を返す
+	//aの国が大きい場合0を返す
 	if (a_total > b_total){
-		Nchange = 1;
-	}
-	else{
 		Nchange = 0;
 	}
+	else{
+		Nchange = 1;
+	}
 	return Nchange;
-	
 }
 

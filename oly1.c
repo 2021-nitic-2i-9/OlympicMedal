df512f0ca769b28201a61468fed79c84c59f3fa5
gaku-hasamaru <st20179gk@gm.ibaraki-ct.ac.jp>
2021-12-09 15:38:18 +0900

    メダル順位による比較

---
+++ oly1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>  
+int check_sum_medal(int aG,int aS,int aB,int bG,int bS,int bB){   //G=金,S=銀,B=銅//
+int Lchange;
+
+if (aG<bG)  //aとbの金メダルの比較：
+{
+    Lchange=1;
+}
+else if (aG==bG)
+{
+    if (aS<bS)
+    {
+        Lchange=1;
+    }
+    else if (aS==bS)
+    {
+        if (aB<bB)
+        {
+           Lchange=1;
+        }
+    }    
+}
+
+    return change;
+}

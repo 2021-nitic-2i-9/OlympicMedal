25eef3288a56ab47caf27aafc20cf15762c2962e
altivelis <ayupon89821026@gmail.com>
2021-12-09 15:34:35 +0900

    fix alpha.c

--- alpha.c
+++ alpha.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 int alpha(char a[64],char b[64]){
-    if(strcmp(a,b)==0) return 2;    //全く同じ文字列だった場合に2を出力
+    if(strcmp(a,b)==0) return 0;    //全く同じ文字列だった場合に2を出力
     int n=0;
     while(1){                       //ここから無限ループ
         if(a[n] > b[n]) return 1;   //アルファベット順で遅い方が左の入力だった場合に1を出力。

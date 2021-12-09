240db876e6a6f9ed5438b2793c94c4558901b55a
altivelis <ayupon89821026@gmail.com>
2021-12-09 15:54:48 +0900

    add break

--- alpha.c
+++ alpha.c
@@ -7,9 +7,10 @@ int alpha(char a[64],char b[64]){
         if(a[n] > b[n]) return 1;   //アルファベット順で遅い方が左の入力だった場合に1を出力。
         if(b[n] > a[n]) return 0;
         else{
-            if(n>64) return 0;
+            if(n>64) break;
             n++;                    //同じ文字だったら次の文字へ
             continue;               //ここまで無限ループ
         }
+        return 0;
     }
 }

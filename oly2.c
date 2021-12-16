56b07d1a51636bd08aeab412797c215cd3eea37c
gaku-hasamaru <st20179gk@gm.ibaraki-ct.ac.jp>
2021-12-16 15:20:25 +0900

    国指定のデータ出せるようにしました

---
+++ oly2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include "type.h"
+char *lower(char *str);
+int namecountry(type_country_data *data, int num) {
+    int much,i;
+    char name[30];
+    scanf("%s",name);
+    for ( i = 0; i < num; i++)
+    {
+        much=strcmp(lower(name),lower(data[i].name));
+        if (much==0)
+        {
+            break;
+        }
+    }
+    if (much!=0)
+    {
+        printf("データがありません。");
+    }
+
+return i;
+}
